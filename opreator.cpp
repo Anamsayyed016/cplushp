58a9f579767bec2d411f936e7c0c8a38bcbe56f2
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-23 19:21:37 +0530

    c++h1

--- opreator.cpp
+++ opreator.cpp
@@ -8,15 +8,40 @@ int main()
 
     // Arthmethic Opreators
 
-    cout<<"Value is a+b is :"<<a+b<<endl;
-    cout<<"Value is a-b is :"<<a-b<<endl;
-    cout<<"Value is a*b is :"<<a*b<<endl;
-    cout<<"Value is a/b is :"<<a/b<<endl;
-    cout<<"Value is a%b is :"<<a%b<<endl;
-    cout<<"Value is ++a is :"<<++a<<endl;
-    cout<<"Value is --a is :"<<--a<<endl;
-    cout<<"Value is --b is :"<<--b<<endl;
+    // cout<<"Value is a+b is :"<<a+b<<endl;
+    // cout<<"Value is a-b is :"<<a-b<<endl;
+    // cout<<"Value is a*b is :"<<a*b<<endl;
+    // cout<<"Value is a/b is :"<<a/b<<endl;
+    // cout<<"Value is a%b is :"<<a%b<<endl;
+    // cout<<"Value is ++a is :"<<++a<<endl;
+    // cout<<"Value is --a is :"<<--a<<endl;
+    // cout<<"Value is --b is :"<<--b<<endl;
 
 
             // Assignment Opreator ==== used to assign  values to variables
-}
+            // int a=8 , b=10;
+            // char d= 'd';
+
+            // Comparison opreator
+        
+    // cout<<"Value is a==b is :"<<(a==b)<<endl;
+    // cout<<"Value is a!=b is :"<<(a!=b)<<endl;
+    // cout<<"Value is a<=b is :"<<(a<=b)<<endl;
+    // cout<<"Value is a>=b is :"<<(a>=b)<<endl;
+    // cout<<"Value is a>b is :"<<(a>b)<<endl;
+    // cout<<"Value is a<b is :"<<(a<b)<<endl;
+
+    // Logical Opreator
+    // cout<<"Value is a==b is :"<<((a==b) && (a<b))<<endl;
+    // cout<<"Value is a&&b is :"<<((a==b) || (a<b))<<endl;
+    // cout<<"Value is ! a==b is :"<<(!(a==b))<<endl;
+   
+//    .................................................
+
+        float d=34.4F;
+        long double e=34.4L;
+ cout<<"The size of 34.4 is :"<<sizeof(34.4)<<endl;
+cout<<"The size of 34.4f is :"<<sizeof(34.4f)<<endl;
+cout<<"The size of 34.4F is :"<<sizeof(34.4F)<<endl;
+cout<<"The size of 34.4l is :"<<sizeof(34.4l)<<endl;
+cout<<"The size of 34.4L is :"<<sizeof(34.4L)<<endl;}
