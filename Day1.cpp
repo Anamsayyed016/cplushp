df7efc42f469c69a6cd8ea61a32b308a2ece6c85
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-23 18:32:10 +0530

    c++h

---
+++ Day1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout<<"Enter the value :\n"; /*<< is called  Insertion opreator */
+    cin>>num1; /*<< is called Extraction opreator*/
+
+    cout<<"Enter the value :\n"; /*<< is called  Insertion opreator */
+    cin>>num2; /*<< is called Extraction opreator*/
+
+    cout<< "The sum is : "<< num1+num2;
+}
---
+++ opreator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=20 ,b=30;
+    cout<<"Opeators  in C++ :"<<endl;
+    cout<<"Types of opreators"<<endl;   
+
+    // Arthmethic Opreators
+
+    cout<<"Value is a+b is :"<<a+b<<endl;
+    cout<<"Value is a-b is :"<<a-b<<endl;
+    cout<<"Value is a*b is :"<<a*b<<endl;
+    cout<<"Value is a/b is :"<<a/b<<endl;
+    cout<<"Value is a%b is :"<<a%b<<endl;
+    cout<<"Value is ++a is :"<<++a<<endl;
+    cout<<"Value is --a is :"<<--a<<endl;
+    cout<<"Value is --b is :"<<--b<<endl;
+
+
+            // Assignment Opreator ==== used to assign  values to variables
+}
