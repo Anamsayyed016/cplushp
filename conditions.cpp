9d1d25d8f2b5237e3a9dfbb0d77393f05c0ab9ec
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-24 18:09:26 +0530

    24c++

---
+++ conditions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a;
+   cout<<"Enter your age"<<endl;
+   cin>>a;
+
+   if(a<18)
+   {
+    cout<<"you are not elegiable"<<endl;
+   }
+   else if(a==18)
+   {
+    cout<<"you are elegiable"<<endl;
+
+   }
+   else
+   {
+    cout<<"you are elegiable"<<endl;
+
+   }
+}
