9b7198cfba0ea150206c08d09c66cff01db1d8af
anamsayyed016 <anamsayyed58@gmail.com>
2025-01-30 15:18:48 +0530

    30c++

---
+++ cal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    char cal;
+    int a,b;
+    cout<<"enter your choice  + , - ,*,/"<<endl;
+    cin>>cal;
+
+    switch(cal)
+    {
+        case 1:cout<<"addition"<<endl;
+        cout<<"addition"<<a+b<<endl;
+        break;
+
+        case 2:cout<<"addition"<<endl;
+        cout<<"addition"<<a+b<<endl;
+        break;
+
+        
+    }
+}
