65d03de0c408e87279c509ce5edc183ad8b23a24
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-03 18:11:11 +0530

    3c++

---
+++ phelnfrom.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num, rev=0 ans;
+    cout<<"Enter a number: ";
+    cin>>num;
+    ans==rev;
+    while (num>0)
+    {
+        int digit = num%10;
+        rev = rev*10 + digit;
+        num = num/10;
+    }
+    if(ans==rev)
+    {
+        cout<<"The number is a palindrome.";
+    }
+    else
+    {
+        cout<<"The number is not a palindrome.";
+    }
+}
