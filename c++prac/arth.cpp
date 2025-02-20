3b16bfbb335cfa8d8b0220cf44183c3547d349fc
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-20 19:18:25 +0530

    20febc++

---
+++ c++prac/arth.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter  your values :";
+    cin>>a>>b;
+
+        cout<< "Add : "<< a+b<<endl;
+        cout<< "Sub : "<< a-b<<endl;
+}
