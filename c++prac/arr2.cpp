cfbccc6e0b4ca4a4d26db85ddc5acd2c93b1dcb6
anamsayyed016 <anamsayyed58@gmail.com>
2025-02-28 02:38:17 +0530

    28febc++

---
+++ c++prac/arr2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,8,4,5,6,9};
+
+        int s=sizeof(arr)/sizeof(arr[5]);
+
+        for(int i=0; i<s;i++ ){
+            cout<<arr[i]<<"\t";
+        }
+}
