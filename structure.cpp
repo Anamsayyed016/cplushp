467ca24a0c1bcb77188e17eb9ed2ae5c3f0d1a7c
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-03 15:07:48 +0530

    3febc++

---
+++ structure.cpp
@@ -0,0 +1,23 @@
+// Structure:- user define datatype.store diffter type of like function and variables
+// syn:-
+
+// int main()
+// {
+    // demo a;
+    // a.b;
+    // a.show()
+// }
+
+// Struct demo{
+
+        // int a;
+        // char b
+        // string s= "anam" 
+        // void show()
+        // { cout<<"hello"};
+
+
+        // [.] -- member opreator
+//}
+
+
