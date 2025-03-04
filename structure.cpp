ce5e2f1d66fbd4e37f225656c7477c2617cb3ad3
anamsayyed016 <anamsayyed58@gmail.com>
2025-03-04 15:24:39 +0530

    4febc++

--- structure.cpp
+++ structure.cpp
@@ -1,4 +1,5 @@
 // Structure:- user define datatype.store diffter type of like function and variables
+// ->= this opreator.when data is pointer for calling we use 'THIS' opreator.
 // syn:-
 
 // int main()
@@ -18,6 +19,44 @@
 
 
         // [.] -- member opreator
-//}
+
+
+// Self Refernial Structure:- refernial own data. and access another data use '* pointer'
+
+#include <iostream>
+using namespace std;
+
+    struct demo{
+        int rollno;
+        string name;
+        char grade;
+        struct demo *link;
+    };
+
+        int main(){
+
+            demo obj1,obj2, obj3;
+            
+            obj1.link=NULL;
+            obj1.rollno=101;
+            obj1.grade='A';
+
+            obj2.link=NULL;
+            obj2.rollno=102;
+            obj2.grade='A';
+
+            obj3.link=NULL;
+            obj1.link=&obj2;
+            obj2.link=&obj1;
+            obj3.link=&obj1;
+
+            cout<<obj1.link->rollno<<endl;
+            cout<<obj2.link->rollno<<endl;
+            cout<<obj3.link->grade;
+
+        }
+
+
+
 
 
